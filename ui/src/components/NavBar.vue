d92a3d56808c9356819ca9fd36418bf0f0150d78
HVEVB <36706634+HVEVB@users.noreply.github.com>
2021-07-04 23:22:47 +0200

    We now have a logo :sunglasses:

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -2,18 +2,37 @@
   <nav v-show="isNavBarVisible" id="navbar-main" class="navbar is-fixed-top">
     <div class="navbar-brand">
       <a class="navbar-item is-hidden-desktop" @click.prevent="menuToggleMobile">
-        <b-icon :icon="menuToggleMobileIcon"/>
+        <b-icon :icon="menuToggleMobileIcon" />
       </a>
       <div class="navbar-item has-control no-left-space-touch">
         <div class="control">
-          <h1>Dojnaz Coupons</h1>
+          <img
+            :style="{
+              height: '3rem',
+              'max-height': '3rem',
+              float: 'left'
+            }"
+            :src="logo"
+          />
+          <h1
+            :style="{
+              'white-space': 'nowrap',
+              float: 'left',
+              'margin-top': '4px',
+              'margin-left': '12.5px',
+              'font-size': '1.5rem'
+            }"
+          >Dojnaz Coupons</h1>
           <!--<input class="input" placeholder="Search everywhere...">-->
         </div>
       </div>
     </div>
     <div class="navbar-brand is-right">
-      <a class="navbar-item navbar-item-menu-toggle is-hidden-desktop" @click.prevent="menuNavBarToggle">
-        <b-icon :icon="menuNavBarToggleIcon" custom-size="default"/>
+      <a
+        class="navbar-item navbar-item-menu-toggle is-hidden-desktop"
+        @click.prevent="menuNavBarToggle"
+      >
+        <b-icon :icon="menuNavBarToggleIcon" custom-size="default" />
       </a>
     </div>
     <div class="navbar-menu fadeIn animated faster" :class="{'is-active':isMenuNavBarActive}">
@@ -42,7 +61,7 @@
           </div>
         </nav-bar-menu>-->
         <nav-bar-menu class="has-divider has-user-avatar">
-          <user-avatar/>
+          <user-avatar />
           <div class="is-user-name">
             <span>{{ userName }}</span>
           </div>
@@ -59,7 +78,7 @@
               <b-icon icon="email" custom-size="default"></b-icon>
               <span>Messages</span>
             </a>
-            <hr class="navbar-divider">
+            <hr class="navbar-divider" />
             <a class="navbar-item">
               <b-icon icon="logout" custom-size="default"></b-icon>
               <span>Log Out</span>
@@ -75,7 +94,7 @@
           <span>About</span>
         </a>-->
         <a class="navbar-item is-desktop-icon-only" title="Log out" @click="logout">
-          <b-icon icon="logout" custom-size="default"/>
+          <b-icon icon="logout" custom-size="default" />
           <span>Log out</span>
         </a>
       </div>
@@ -84,59 +103,61 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import NavBarMenu from '@/components/NavBarMenu'
-import UserAvatar from '@/components/UserAvatar'
+import { mapState } from "vuex";
+import NavBarMenu from "@/components/NavBarMenu";
+import UserAvatar from "@/components/UserAvatar";
+import logo from "@/logo.png";
 
 export default {
-  name: 'NavBar',
+  name: "NavBar",
   components: {
     UserAvatar,
-    NavBarMenu
+    NavBarMenu,
   },
-  data () {
+  data() {
     return {
-      isMenuNavBarActive: false
-    }
+      isMenuNavBarActive: false,
+      logo: logo,
+    };
   },
   computed: {
-    menuNavBarToggleIcon () {
-      return (this.isMenuNavBarActive) ? 'close' : 'dots-vertical'
+    menuNavBarToggleIcon() {
+      return this.isMenuNavBarActive ? "close" : "dots-vertical";
     },
-    menuToggleMobileIcon () {
-      return this.isAsideMobileExpanded ? 'backburger' : 'forwardburger'
+    menuToggleMobileIcon() {
+      return this.isAsideMobileExpanded ? "backburger" : "forwardburger";
     },
-    darkModeToggleIcon () {
-      return this.isDarkModeActive ? 'white-balance-sunny' : 'weather-night'
+    darkModeToggleIcon() {
+      return this.isDarkModeActive ? "white-balance-sunny" : "weather-night";
     },
     ...mapState([
-      'isNavBarVisible',
-      'isAsideMobileExpanded',
-      'isDarkModeActive',
-      'userName'
-    ])
+      "isNavBarVisible",
+      "isAsideMobileExpanded",
+      "isDarkModeActive",
+      "userName",
+    ]),
   },
-  mounted () {
+  mounted() {
     this.$router.afterEach(() => {
-      this.isMenuNavBarActive = false
-    })
+      this.isMenuNavBarActive = false;
+    });
   },
   methods: {
-    menuToggleMobile () {
-      this.$store.commit('asideMobileStateToggle')
+    menuToggleMobile() {
+      this.$store.commit("asideMobileStateToggle");
     },
-    menuNavBarToggle () {
-      this.isMenuNavBarActive = (!this.isMenuNavBarActive)
+    menuNavBarToggle() {
+      this.isMenuNavBarActive = !this.isMenuNavBarActive;
     },
-    darkModeToggle () {
-      this.$store.commit('darkModeToggle')
+    darkModeToggle() {
+      this.$store.commit("darkModeToggle");
     },
-    logout () {
+    logout() {
       this.$buefy.snackbar.open({
-        message: 'Log out clicked',
-        queue: false
-      })
-    }
-  }
-}
+        message: "Log out clicked",
+        queue: false,
+      });
+    },
+  },
+};
 </script>
